25b54f7429db0d183705fa94568f9821319bee76
Agustin Barto <abarto@gmail.com>
2015-02-14 12:05:40 -0300

    Register incident model on admin site

--- tracker_project/tracker/admin.py
+++ tracker_project/tracker/admin.py
@@ -1,3 +1,8 @@
+from __future__ import absolute_import, unicode_literals
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import Incident
+
+
+admin.site.register(Incident)
