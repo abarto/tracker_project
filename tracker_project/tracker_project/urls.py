b6dcb4029d3bf4b402a6874c942c9e4a105f2a62
Agustin Barto <abarto@gmail.com>
2015-02-14 20:54:30 -0300

    Fix login and logout URLs

--- tracker_project/tracker_project/urls.py
+++ tracker_project/tracker_project/urls.py
@@ -1,11 +1,18 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.core.urlresolvers import reverse_lazy
 
 
 urlpatterns = patterns(
     '',
+    url(r'^$', 'tracker_project.views.home', name='home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('django.contrib.auth.urls')),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
-    url(r'^', 'tracker_project.views.home', name='home')
+    url(
+        r'^logout/$',
+        'django.contrib.auth.views.logout',
+        {'next_page': reverse_lazy('home')},
+        name='logout'
+    ),
+    url(r'^tracker/', include('tracker.urls', 'tracker')),
 )
