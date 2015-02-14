1a457edfc7ccebc63ac2341d05542bc119cc42b3
Agustin Barto <abarto@gmail.com>
2015-02-14 10:48:57 -0300

    Switch to PostgreSQL database backend

--- tracker_project/tracker_project/settings.py
+++ tracker_project/tracker_project/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'tracker_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tracker_project',
+        'USER': 'tracker_project',
+        'PASSWORD': 'tracker_project',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
