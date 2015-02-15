d2fbad5c2ae21f9f98fd5d5a8467a05f52343af8
Agustin Barto <abarto@gmail.com>
2015-02-15 00:57:48 -0300

    Add info window to map

--- tracker_project/assets/js/incident_form.js
+++ tracker_project/assets/js/incident_form.js
@@ -1,5 +1,6 @@
 var map = null;
 var marker = null;
+var infoWindow = null;
 
 function setMarker(latLng, map) {
     if (marker !== null) {
@@ -24,10 +25,28 @@ function initializeMap(locationLatitude, locationLongitude, zoomLevel) {
         zoom: zoomLevel
     };
 
+    var infoWindow = new google.maps.InfoWindow({
+        content: ''
+    });
+
     map = new google.maps.Map($("#map-canvas").get(0), mapOptions);
     setMarker(locationLatlng, map);
 
-    google.maps.event.addListener(map, 'click', function(event) {
+    google.maps.event.addListener(map, "click", function(event) {
         setMarker(event.latLng, map);
+    });
+    google.maps.event.addListener(map, "rightclick", function(event) {
+        var latLng = event.latLng;
+
+        $infoWindowContent = $("<div><span>Latitude: " + latLng.lat() + "</span><br/><span>Longitude: " + latLng.lng() + "</span><br/>" + "<a class=\"btn btn-default btn-xs btn-block\">Place marker</a></div>");
+        $infoWindowContent.find("a").click(function() {
+            infoWindow.close();
+            setMarker(infoWindow.getPosition(), map);
+        });
+
+        infoWindow.setContent($infoWindowContent.get(0));
+        infoWindow.setPosition(event.latLng);
+
+        infoWindow.open(map);
     });
 }
