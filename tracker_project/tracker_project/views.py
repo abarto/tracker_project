e4154644e8ba29b395846b7843deb7bb312508df
Agustin Barto <abarto@gmail.com>
2015-02-19 22:48:30 -0300

    Fix home view

--- tracker_project/tracker_project/views.py
+++ tracker_project/tracker_project/views.py
@@ -1,19 +1,9 @@
 from __future__ import absolute_import, unicode_literals
 
-from django.contrib.auth.decorators import login_required
+from braces.views import LoginRequiredMixin
 from django.views.generic import TemplateView
 
-from tracker.models import Incident, AreaOfInterest
 
-
-class HomeView(TemplateView):
+class HomeView(LoginRequiredMixin, TemplateView):
     template_name = 'tracker_project/home.html'
-
-    def get_context_data(self, **kwargs):
-        context = super(HomeView, self).get_context_data(**kwargs)
-
-        context['incidents'] = Incident.objects.filter(closed=False)
-        context['areas_of_interest'] = AreaOfInterest.objects.all()
-
-        return context
-home = login_required(HomeView.as_view())
+home = HomeView.as_view()
